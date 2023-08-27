f5d3a66d33c518126902997cad75bb6cc0c21cdc
Christian Hildenbrand <christian.hildenbrand90@googlemail.com>
2023-08-27 23:56:45 +0200

    add doxygen to toolchain

--- open_project.bat
+++ open_project.bat
@@ -11,6 +11,7 @@ set "COMPILER_ROOT_PATH=%TOOLS_PATH%\ArmGnuToolchain\bin"
 set "ASTYLE_ROOT_PATH=%TOOLS_PATH%\Astyle"
 set "MAKE_ROOT_PATH=%TOOLS_PATH%\MinGW\bin"
 set "OPENOCD_PATH=%TOOLS_PATH%\openocd\bin"
+set "DOXYGEN_PATH=%TOOLS_PATH%\doxygen\bin"
 
 @echo Set execution paths...
 set "ECLIPSE=%ECLIPSE_ROOT_PATH%\eclipse.exe"
@@ -21,6 +22,7 @@ set "GDB=%COMPILER_ROOT_PATH%\arm-none-eabi-gdb.exe"
 set "ASTYLE=%ASTYLE_ROOT_PATH%\astyle.exe"
 set "MAKE_EXE=%MAKE_ROOT_PATH%\mingw32-make.exe"
 set "OPENOCD=%OPENOCD_PATH%\openocd.exe"
+Set "DOXYGEN=%DOXYGEN_PATH%\doxygen.exe"
 
 REM Set Project Paths
 @echo Set project paths...
