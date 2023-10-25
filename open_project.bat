e1649e15b829c7191f1941d4690db25f0e924278
Christian Hildenbrand <christian.hildenbrand90@googlemail.com>
2023-10-25 22:14:09 +0200

    add path to Doxyfile and to pre_process script

--- open_project.bat
+++ open_project.bat
@@ -31,7 +31,9 @@ set "PROJECT_ROOT_PATH=%cd%"
 set "WORKSPACE_PATH=%PROJECT_ROOT_PATH%\eclipse"
 set "PROJECT_PATH=%WORKSPACE_PATH%\%PROJECT_NAME%"
 set "SVD_FILE_PATH=%PROJECT_ROOT_PATH%\source\lib\STM32G474xx.svd"
+set "PRE_PROCESS=%PROJECT_ROOT_PATH%\tools\pre_process.py"
 Set "POST_PROCESS=%PROJECT_ROOT_PATH%\tools\post_process.bat"
+set "DOXYFILE_PATH=%PROJECT_PATH%\Doxyfile.txt"
 
 REM Add root paths to PATH enviroment variables
 
