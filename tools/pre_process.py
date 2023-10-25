e1611ed8e340bc326862747e8271181f0db819cc
Christian Hildenbrand <christian.hildenbrand90@googlemail.com>
2023-10-25 22:13:47 +0200

    add verbosity of astyled files

--- tools/pre_process.py
+++ tools/pre_process.py
@@ -1,23 +1,26 @@
-# post processing hex file by padding, aligning and inserting crc-32 at last 4 bytes
-
-# set PATH=%PATH%;C:\Users\AsusV\AppData\Local\Programs\Python\Python311\Scripts
-# py -m pip install --upgrade pip
 
 import subprocess
 from sys import argv
 from glob import glob
 
-print("Preprocessing files...")
-
 # parse arguments and concartinate normalized absolute path to hex file
 proj_root_dir = argv[1]
 astyle_exe_path = argv[2]
+
+verbose = False
+if len(argv) > 3:
+    if (argv[3] == '--verbose') | (argv[3] == '-v'):
+        verbose = True
+   
 
 pattern = ['source/**/*.c', "source/**/*.h"]
 rel_paths = []
 
 for item in pattern:
     rel_paths += glob(item, root_dir=proj_root_dir, recursive=True)
+
+if verbose:
+    print("Astyle imposed on: ", rel_paths)
 
 for item in rel_paths:
     proc = subprocess.run(args=[astyle_exe_path, '--style=allman', '-q', '-n', item], cwd=proj_root_dir)
