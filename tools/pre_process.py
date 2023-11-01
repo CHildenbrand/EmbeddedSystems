6ab87b23d7c46f24b51c49d1f7e0c1453cf62534
Christian Hildenbrand <christian.hildenbrand90@googlemail.com>
2023-11-01 22:54:39 +0100

    impose new astyle config; relocate Doxyfile; add Astylefile
    add --options=<FileToAstylefile> in python script

--- tools/pre_process.py
+++ tools/pre_process.py
@@ -6,12 +6,12 @@ from glob import glob
 # parse arguments and concartinate normalized absolute path to hex file
 proj_root_dir = argv[1]
 astyle_exe_path = argv[2]
+astyle_cfg = argv[3]
 
 verbose = False
-if len(argv) > 3:
-    if (argv[3] == '--verbose') | (argv[3] == '-v'):
+if len(argv) > 4:
+    if (argv[4] == '--verbose') | (argv[4] == '-v'):
         verbose = True
-   
 
 pattern = ['source/**/*.c', "source/**/*.h"]
 rel_paths = []
@@ -19,8 +19,15 @@ rel_paths = []
 for item in pattern:
     rel_paths += glob(item, root_dir=proj_root_dir, recursive=True)
 
+rel_paths.sort()
+
+optionFile = '--options='
+optionFile += astyle_cfg
+
 for item in rel_paths:
-    proc = subprocess.run(args=[astyle_exe_path, '--style=allman', '-q', '-n', item], cwd=proj_root_dir)
+    proc = subprocess.run(args=[astyle_exe_path, optionFile, item], cwd=proj_root_dir)
     
 if verbose:
-    print("Formatting List: \n", rel_paths)
+    print("Formatting List:")
+    for item in rel_paths:
+        print(item)
